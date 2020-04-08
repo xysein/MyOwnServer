602a83e50f33389b51597ad6942d05624ca497b6
Artsiom_Panko <artsiom_panko@epam.com>
2020-04-08 14:07:54 +0300

    Move from usual HTML to Freemarker

--- src/main/java/com/example/MyWebServer/controllers/MainController.java
+++ src/main/java/com/example/MyWebServer/controllers/MainController.java
@@ -11,43 +11,47 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Map;
+
 @Controller
 public class MainController {
-
     @Autowired
     private MessageRepository messageRepository;
 
+    @GetMapping("/")
+    public String greeting(Map<String, Object> model) {
+        return "greeting";
+    }
 
     @GetMapping("/main")
-    public String main(Model model) {
-        model.addAttribute("messages", messageRepository.findAll());
+    public String main(@RequestParam(required = false, defaultValue = "") String filter, Model model) {
+        Iterable<Message> messages = messageRepository.findAll();
+
+        if (filter != null && !filter.isEmpty()) {
+            messages = messageRepository.findByTag(filter);
+        } else {
+            messages = messageRepository.findAll();
+        }
+
+        model.addAttribute("messages", messages);
+        model.addAttribute("filter", filter);
+
         return "main";
     }
 
     @PostMapping("/main")
-    public String addMessage(
+    public String add(
             @AuthenticationPrincipal User user,
             @RequestParam String text,
-            @RequestParam String tag, Model model) {
+            @RequestParam String tag, Map<String, Object> model
+    ) {
+        Message message = new Message(text, tag, user);
 
-        messageRepository.save(new Message(text, tag, user));
+        messageRepository.save(message);
 
-        model.addAttribute("messages", messageRepository.findAll());
+        Iterable<Message> messages = messageRepository.findAll();
 
-        return "main";
-    }
-
-    @PostMapping("filter")
-    public String filter(@RequestParam String filter, Model model) {
-        Iterable<Message> messageList;
-
-        if (filter == null || filter.trim().isEmpty()) {
-            messageList = messageRepository.findAll();
-        } else {
-            messageList = messageRepository.findByTag(filter);
-        }
-
-        model.addAttribute("messages", messageList);
+        model.put("messages", messages);
 
         return "main";
     }
